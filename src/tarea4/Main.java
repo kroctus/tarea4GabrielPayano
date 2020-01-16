877b1b59e0cb01aedaddb648a1d4bd2c9359b69f
AndreiChirigut <andreichirigut07@gmail.com>
2020-01-16 18:17:49 +0100

    Añade la clase Main con los objetos

---
+++ src/tarea4/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4;
+
+/**
+ *
+ * @author Chiri
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Empleado em1 = new Empleado("Manolo", "Burgos", 3);
+        Empleado em2 = new Empleado("Maria", "Lopez", 0);
+              
+        System.out.println(em1);
+        System.out.println(em2);
+    }
+}
