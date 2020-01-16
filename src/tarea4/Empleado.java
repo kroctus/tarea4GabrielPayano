25cfa748aaa44f80d4a2e03d1db0b82a4e494b2a
gabriel_payano <gabriel21kun@gmail.com>
2020-01-16 18:07:27 +0100

    "Añadimos getter and setters"

--- src/tarea4/Empleado.java
+++ src/tarea4/Empleado.java
@@ -30,6 +30,33 @@ public class Empleado {
         this.numHijos=0;
     }
     
+    //getter and setter
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNumHijos() {
+        return numHijos;
+    }
+
+    public void setNumHijos(int numHijos) {
+        this.numHijos = numHijos;
+    }
+    
+    
     
     
     
